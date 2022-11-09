1cd964362ebe22e5568ecf933417f4da9cbef3ec
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-11-09 16:59:52 -0500

    feat: implement functionality to get alternate name of note

---
+++ server/helpers/getAlternate.js
@@ -0,0 +1,45 @@
+const { get } = require("../app");
+
+const getAlternate = note => {
+  const naturals = ["C", "D", "E", "F", "G", "A", "B"];
+  const { octave, letter, accidental } = note;
+
+  // Edge case where note is natural
+  if (accidental === "natural") {
+    return note;
+  }
+
+  // Edge case where alternate would be a note in a different octave
+  if (
+    (letter === "C" && accidental === "flat") ||
+    (letter === "B" && accidental === "sharp")
+  ) {
+    return false;
+  }
+
+  // Edge case where alternate is natural
+  if (letter === "E" && accidental === "sharp") {
+    return { octave, letter: "F", accidental: "natural" };
+  } else if (letter === "F" && accidental === "flat") {
+    return { octave, letter: "E", accidental: "natural" };
+  }
+
+  const position = naturals.findIndex(natural => natural === note.letter);
+  if (accidental === "sharp") {
+    return {
+      octave,
+      letter: naturals[position + 1],
+      accidental: "flat",
+    };
+  }
+
+  if (accidental === "flat") {
+    return {
+      octave,
+      letter: naturals[position - 1],
+      accidental: "sharp",
+    };
+  }
+};
+
+module.exports = getAlternate;
