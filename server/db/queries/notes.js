2acdab0c7ad8701f941e5f8a201414fbb124c827
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-11-09 15:08:55 -0500

    feat: implement route for fetching single note

--- server/db/queries/notes.js
+++ server/db/queries/notes.js
@@ -1,9 +1,22 @@
 const db = require("../../config/db.config");
 
 const getAllNotes = () => {
-  return db.query("SELECT * FROM notes;").then(data => {
-    return data.rows;
-  });
+  return db.query("SELECT * FROM notes;").then(data => data.rows);
 };
 
-module.exports = { getAllNotes };
+const getNote = (octave, letter, accidental = "natural") => {
+  return db
+    .query(
+      "SELECT * FROM notes WHERE octave_id = $1 AND letter = $2 AND accidental = $3;",
+      [octave, letter.toUpperCase(), accidental]
+    )
+    .then(data => data.rows);
+};
+
+const getOctave = octave => {
+  return db
+    .query("SELECT * FROM notes WHERE octave_id = $1;", [octave])
+    .then(data => data.rows);
+};
+
+module.exports = { getAllNotes, getOctave, getNote };
--- server/routes/notes.js
+++ server/routes/notes.js
@@ -3,8 +3,30 @@ const notes = require("../db/queries/notes");
 
 router.get("/", (req, res) => {
   notes.getAllNotes().then(data => {
-    res.json({ notes: data });
+    const notes = {};
+    data.forEach(note => {
+      if (notes[note.octave_id]) {
+        notes[note.octave_id].push(note);
+      } else {
+        notes[note.octave_id] = [note];
+      }
+    });
+    return res.json({ notes });
   });
+});
+
+router.get("/octaves/:octave", (req, res) => {
+  const { octave } = req.params;
+  notes.getOctave(octave).then(data => {
+    return res.json({ notes: data });
+  });
+});
+
+router.get("/:octave/:letter/:accidental?", (req, res) => {
+  const { octave, letter, accidental } = req.params;
+  notes
+    .getNote(octave, letter, accidental)
+    .then(data => res.json({ note: data }));
 });
 
 module.exports = router;
