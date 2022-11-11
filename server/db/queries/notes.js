3cb1b951e8f4f85b338cf55e73dd0b2f31a26868
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-11-10 21:10:26 -0500

    fix: catch errors

--- server/db/queries/notes.js
+++ server/db/queries/notes.js
@@ -1,7 +1,10 @@
 const db = require("../../config/db.config");
 
 const getAllNotes = () => {
-  return db.query("SELECT * FROM notes;").then(data => data.rows);
+  return db
+    .query("SELECT * FROM notes;")
+    .then(data => data.rows)
+    .catch(err => err);
 };
 
 const getNote = (octave, letter, accidental = "natural") => {
@@ -10,13 +13,15 @@ const getNote = (octave, letter, accidental = "natural") => {
       "SELECT * FROM notes WHERE octave_id = $1 AND letter = $2 AND accidental = $3;",
       [octave, letter.toUpperCase(), accidental]
     )
-    .then(data => data.rows);
+    .then(data => data.rows)
+    .catch(err => err);
 };
 
 const getOctave = octave => {
   return db
     .query("SELECT * FROM notes WHERE octave_id = $1;", [octave])
-    .then(data => data.rows);
+    .then(data => data.rows)
+    .catch(err => err);
 };
 
 module.exports = { getAllNotes, getOctave, getNote };
