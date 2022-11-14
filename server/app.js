aa214266cf8065f12987f8e90c7039ec9a9e11e1
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-11-14 18:17:34 -0500

    feat: setup index route

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const logger = require("morgan");
 
+const indexRouter = require("./routes/index");
 const notesRouter = require("./routes/notes");
 const octavesRouter = require("./routes/octaves");
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use("/", indexRouter);
 app.use("/notes", notesRouter);
 app.use("/octaves", octavesRouter);
 
